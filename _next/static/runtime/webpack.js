dea91dbc867bef64ca7dc98fac78b56a1f08a38e
micah rich <micah@micahrich.com>
2019-09-11 17:17:17 -0400

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fee160f3166ea01e3435";
+/******/ 	var hotCurrentHash = "777f99e1e27333834631";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ _next/static/webpack/static/development/pages/index.js.dcd9567da6a76d7eee7f.hot-update.js
@@ -0,0 +1,246 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/index.js":
+/*!*****************************!*\
+  !*** ./components/index.js ***!
+  \*****************************/
+/*! exports provided: Banner, Flex, Right, Left, Button, Section, Intro, Quote */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Banner", function() { return Banner; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Flex", function() { return Flex; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Right", function() { return Right; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Left", function() { return Left; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Button", function() { return Button; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Section", function() { return Section; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Intro", function() { return Intro; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Quote", function() { return Quote; });
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_extends__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/extends */ "./node_modules/@babel/runtime-corejs2/helpers/esm/extends.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_objectWithoutProperties__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/objectWithoutProperties */ "./node_modules/@babel/runtime-corejs2/helpers/esm/objectWithoutProperties.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/taggedTemplateLiteral */ "./node_modules/@babel/runtime-corejs2/helpers/esm/taggedTemplateLiteral.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var _emotion_styled__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @emotion/styled */ "./node_modules/@emotion/styled/dist/styled.browser.esm.js");
+/* harmony import */ var _emotion_core__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @emotion/core */ "./node_modules/@emotion/core/dist/core.browser.esm.js");
+
+
+
+var _jsxFileName = "/Users/micahbrich/Sites/lexend/components/index.js";
+
+
+function _templateObject12() {
+  var data = Object(_babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_2__["default"])(["\n  background: red;\n  padding: 20vh 10vw;\n  h1 {\n    animation: ", " 900ms ease-in-out;\n    font-size: 3.2vw;\n    line-height: 1.392;\n    color: white;\n    display: flex;\n    flex-direction: column;\n    flex-wrap: wrap;\n    span {\n      display: inline-block;\n    }\n    strong {\n      color: white;\n    }\n    @media (max-width: 960px) {\n      font-size: 2.1rem;\n    }\n  }\n  @media (max-width: 960px) {\n    padding: 8vh 6vw;\n  }\n"]);
+
+  _templateObject12 = function _templateObject12() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject11() {
+  var data = Object(_babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_2__["default"])(["\n  margin: 0.618rem 1.618rem;\n  flex: 1 1 45%;\n  max-width: 100%;\n  position: relative;\n  &:nth-of-type(1) {\n    .image {\n      order: 0;\n    }\n  }\n  blockquote {\n    box-shadow: 0.392rem 0.618rem 1rem rgba(0, 0, 0, 0.12);\n    display: flex;\n    flex-direction: row;\n    margin-top: 0;\n    max-width: 760px;\n    background: white;\n    padding: 0;\n    margin: 4rem auto 1rem;\n    position: relative;\n    border: 0.618rem solid red;\n    p {\n      flex: 1 1 auto;\n      font-size: 1rem;\n      line-height: 1.618;\n      margin: 3rem 1rem 1rem;\n    }\n    .image {\n      order: 2;\n      display: block;\n      width: 100%;\n      min-width: 200px;\n      max-width: 530px;\n      background: url(", ") no-repeat center center;\n      background-size: cover;\n      img {\n        display: none;\n      }\n      @media (max-width: 960px) {\n        background: none;\n        max-width: 100%;\n        order: 1 !important;\n        img {\n          display: block;\n          width: 100%;\n        }\n      }\n    }\n    h6 {\n      max-width: 100%;\n      margin: 0 1rem 3rem;\n      font-variation-settings: \"LXND\" 20;\n      letter-spacing: 1px;\n      text-transform: uppercase;\n    }\n    &:before {\n      font-weight: normal;\n      content: \"\u201C\";\n      font-size: 9.6rem;\n      position: absolute;\n      top: -4.618rem;\n      left: 1.618rem;\n      color: black;\n    }\n    &:after {\n      font-weight: normal;\n      content: \"\u201D\";\n      font-size: 9.6rem;\n      position: absolute;\n      bottom: -8.618rem;\n      right: 1.618rem;\n      color: black;\n    }\n    @media (max-width: 1220px) {\n      max-width: 100%;\n      left: 0;\n      left: 0;\n    }\n    @media (max-width: 640px) {\n      display: block;\n      background: none !important;\n      padding: 0 !important;\n      .image {\n        max-width: 100%;\n      }\n    }\n  }\n"]);
+
+  _templateObject11 = function _templateObject11() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject10() {
+  var data = Object(_babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_2__["default"])(["\n      margin: ", ";\n    "]);
+
+  _templateObject10 = function _templateObject10() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject9() {
+  var data = Object(_babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_2__["default"])(["\n  padding: 1rem;\n  max-width: 1440px;\n  margin: 0 auto;\n  ", "\n  h1 {\n    font-size: 7.2vw;\n    line-height: 1.1;\n    margin: 14vh 3.618vw 3.618rem;\n    font-weight: normal;\n    strong {\n      color: red;\n      font-weight: normal;\n    }\n    @media (max-width: 960px) {\n      width: 100%;\n      font-size: 10vw;\n      line-height: 1.2;\n      margin-left: 0;\n      margin-right: 0;\n    }\n  }\n  h2,\n  h3,\n  h4,\n  h5,\n  h6 {\n    font-weight: normal;\n    max-width: 75;\n    margin: 1rem auto 0.618rem;\n    line-height: 1.5;\n  }\n  h2 {\n    font-size: 3.2rem;\n    line-height: 1.3;\n    text-align: center;\n    @media (max-width: 960px) {\n      max-width: 100%;\n      text-align: left;\n      font-size: 3.2rem;\n    }\n  }\n  h3 {\n    margin-top: 7rem;\n    font-size: 2.1rem;\n    @media (max-width: 960px) {\n      font-size: 1.8rem;\n      max-width: 100%;\n    }\n  }\n  h5 {\n    margin-bottom: -3.618rem;\n    margin-top: 3rem;\n    font-size: 1.4rem;\n    strong {\n      color: red;\n      font-weight: normal;\n    }\n    @media (max-width: 960px) {\n      max-width: 100%;\n      font-size: 0.918rem;\n      margin-bottom: -6rem;\n    }\n  }\n  h6 {\n    max-width: 30%;\n    margin: 0 auto;\n    font-variation-settings: \"LXND\" 20;\n    @media (max-width: 960px) {\n      max-width: 88%;\n    }\n    a {\n      font-variation-settings: \"LXND\" 80;\n      text-transform: uppercase;\n      letter-spacing: 2px;\n      text-decoration: none;\n      &:before {\n        content: \"\u2301\";\n        margin: 0 0.192rem;\n        font-size: 140%;\n      }\n    }\n  }\n\n  p {\n    font-variation-settings: \"LXND\" 10;\n    color: rgba(0, 0, 0, 0.76);\n    line-height: 1.6;\n    font-size: 1.2rem;\n    max-width: 800px;\n    margin: 2rem auto;\n  }\n  img {\n    display: block;\n    max-width: 500px;\n  }\n  a {\n    color: red;\n  }\n  ul,\n  ol {\n    max-width: 720px;\n    margin: 1.618rem auto;\n  }\n"]);
+
+  _templateObject9 = function _templateObject9() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject8() {
+  var data = Object(_babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_2__["default"])(["\n  -webkit-appearance: none;\n  font-family: \"Lexend\", Helvetica, Arial, sans-serif;\n  text-transform: uppercase;\n  letter-spacing: 2px;\n  cursor: pointer;\n  appearance: none;\n  border: none;\n  border-radius: 3px;\n  padding: 0.618em 1em;\n  transition: all 300ms ease-in;\n  outline: none;\n  min-width: 70px;\n  background: ", ";\n  color: ", ";\n  @media (max-width: 1020px) {\n    width: 100%;\n    margin-top: 0.392rem;\n  }\n"]);
+
+  _templateObject8 = function _templateObject8() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject7() {
+  var data = Object(_babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_2__["default"])(["\n      position: sticky;\n      top: ", ";\n    "]);
+
+  _templateObject7 = function _templateObject7() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject6() {
+  var data = Object(_babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_2__["default"])(["\n  margin: 2.618rem auto;\n  padding: 0 1rem;\n  max-width: ", ";\n  width: ", ";\n  height: ", ";\n  ", "\n  @media (max-width: 960px) {\n    /* max-width: 100%; */\n    position: static;\n  }\n"]);
+
+  _templateObject6 = function _templateObject6() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject5() {
+  var data = Object(_babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_2__["default"])(["\n      position: sticky;\n      top: ", ";\n    "]);
+
+  _templateObject5 = function _templateObject5() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject4() {
+  var data = Object(_babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_2__["default"])(["\n  margin: 1rem auto;\n  padding: 0 1rem;\n  max-width: ", ";\n  width: ", ";\n  min-width: 400px;\n  ", "\n  @media (max-width: 960px) {\n    /* max-width: 100%; */\n    width: 100%;\n    min-width: auto;\n    position: static;\n  }\n"]);
+
+  _templateObject4 = function _templateObject4() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject3() {
+  var data = Object(_babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_2__["default"])(["\n  display: flex;\n  margin: ", ";\n  align-items: ", ";\n  justify-content: ", ";\n  max-width: 1440px;\n  flex-wrap: ", ";\n  @media (max-width: 960px) {\n    flex-wrap: wrap;\n  }\n"]);
+
+  _templateObject3 = function _templateObject3() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject2() {
+  var data = Object(_babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_2__["default"])(["\n  background: red;\n  color: white;\n  padding: 1rem;\n  text-align: center;\n  margin: 0 auto -6.618rem;\n  font-size: 0.818rem;\n  line-height: 1.6;\n  color: rgba(255, 255, 255, 0.8);\n  strong {\n    color: white;\n    font-variation-settings: \"LXND\" 100;\n    text-transform: uppercase;\n    letter-spacing: 2px;\n    font-size: 85%;\n    padding: 0 0.292rem;\n  }\n  @media (max-width: 640px) {\n    text-align: left;\n  }\n"]);
+
+  _templateObject2 = function _templateObject2() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject() {
+  var data = Object(_babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_2__["default"])(["\n  from {\n    opacity: 0;\n  }\n\n  to {\n    opacity: 1;\n  }\n"]);
+
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+
+
+
+var fadeIn = Object(_emotion_core__WEBPACK_IMPORTED_MODULE_5__["keyframes"])(_templateObject());
+var Banner = Object(_emotion_styled__WEBPACK_IMPORTED_MODULE_4__["default"])("h6")(_templateObject2());
+var Flex = Object(_emotion_styled__WEBPACK_IMPORTED_MODULE_4__["default"])("section")(_templateObject3(), function (props) {
+  return props.margin || "4rem auto";
+}, function (props) {
+  return props.align || "flex-start";
+}, function (props) {
+  return props.justify || "flex-start";
+}, function (props) {
+  return props.wrap || "no-wrap";
+});
+var Right = Object(_emotion_styled__WEBPACK_IMPORTED_MODULE_4__["default"])("main")(_templateObject4(), function (props) {
+  return props.width || "720px";
+}, function (props) {
+  return props.width || "auto";
+}, function (props) {
+  return props.sticky && Object(_emotion_core__WEBPACK_IMPORTED_MODULE_5__["css"])(_templateObject5(), props.top || "1rem");
+});
+var Left = Object(_emotion_styled__WEBPACK_IMPORTED_MODULE_4__["default"])("main")(_templateObject6(), function (props) {
+  return props.width || "720px";
+}, function (props) {
+  return props.width || "auto";
+}, function (props) {
+  return props.height || "auto";
+}, function (props) {
+  return props.sticky && Object(_emotion_core__WEBPACK_IMPORTED_MODULE_5__["css"])(_templateObject7(), props.top || "1rem");
+});
+var Button = Object(_emotion_styled__WEBPACK_IMPORTED_MODULE_4__["default"])("button")(_templateObject8(), function (props) {
+  return props.started ? "white" : "red";
+}, function (props) {
+  return props.started ? "black" : "white";
+});
+var Section = Object(_emotion_styled__WEBPACK_IMPORTED_MODULE_4__["default"])("section")(_templateObject9(), function (props) {
+  return props.margin && Object(_emotion_core__WEBPACK_IMPORTED_MODULE_5__["css"])(_templateObject10(), props.margin);
+});
+var QuoteContainer = Object(_emotion_styled__WEBPACK_IMPORTED_MODULE_4__["default"])("div")(_templateObject11(), function (props) {
+  return props.background;
+});
+var Intro = Object(_emotion_styled__WEBPACK_IMPORTED_MODULE_4__["default"])("section")(_templateObject12(), fadeIn);
+var Quote = function Quote(_ref) {
+  var source = _ref.source,
+      children = _ref.children,
+      background = _ref.background,
+      props = Object(_babel_runtime_corejs2_helpers_esm_objectWithoutProperties__WEBPACK_IMPORTED_MODULE_1__["default"])(_ref, ["source", "children", "background"]);
+
+  return react__WEBPACK_IMPORTED_MODULE_3___default.a.createElement(QuoteContainer, Object(_babel_runtime_corejs2_helpers_esm_extends__WEBPACK_IMPORTED_MODULE_0__["default"])({
+    background: background
+  }, props, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 332
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_3___default.a.createElement("blockquote", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 333
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_3___default.a.createElement("div", {
+    className: "image",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 334
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_3___default.a.createElement("img", {
+    src: background,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 335
+    },
+    __self: this
+  })), react__WEBPACK_IMPORTED_MODULE_3___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 337
+    },
+    __self: this
+  }, children, react__WEBPACK_IMPORTED_MODULE_3___default.a.createElement("h6", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 339
+    },
+    __self: this
+  }, "\u2014 ", source))));
+};
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.dcd9567da6a76d7eee7f.hot-update.js.map
