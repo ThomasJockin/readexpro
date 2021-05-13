7f933fbd10177345d90c76427f11d92cf9312610
Rosalie Wagner <mail@rosaliewagner.com>
2021-05-13 16:38:55 +0200

    muted subsetting

--- sources/build.sh
+++ sources/build.sh
@@ -19,16 +19,16 @@ do
 done 
 
 
-echo ".
-SUBSETTING STATICS
-."
+# echo ".
+# SUBSETTING STATICS
+# ."
 
-ttfs=$(ls $ttDir/*.ttf)
-for font in $ttfs
-do
-	pyftsubset $font --unicodes="0-3c0,1e08-fb02" --layout-scripts=latn,grek --layout-features='*' --output-file=$font.subset --name-IDs='*' --name-legacy --glyph-names --symbol-cmap --legacy-cmap --notdef-glyph --notdef-outline --recommended-glyphs  --drop-tables=
-	mv $font.subset $font
-done 
+# ttfs=$(ls $ttDir/*.ttf)
+# for font in $ttfs
+# do
+# 	pyftsubset $font --unicodes="0-3c0,1e08-fb02" --layout-scripts=latn,grek --layout-features='*' --output-file=$font.subset --name-IDs='*' --name-legacy --glyph-names --symbol-cmap --legacy-cmap --notdef-glyph --notdef-outline --recommended-glyphs  --drop-tables=
+# 	mv $font.subset $font
+# done 
 
 echo ".
 COMPLETE!
