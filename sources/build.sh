adeef464773f3a1d7cdb5b235f6fe112d71abd4d
Rosalie Wagner <mail@rosaliewagner.com>
2021-06-08 17:11:50 +0200

    Completed build system to generate 7 variable families from the same source

--- sources/build.sh
+++ sources/build.sh
@@ -2,21 +2,63 @@
 set -e
 #source ../env/bin/activate
 
-ttDir=../fonts/ttf
-
+vf=Lexend[HEXP,wght].ttf
 
 echo ".
-BUILD FONTS
+BUILD DECA
 ."
 
-gftools builder config.yaml
+gftools builder deca.yaml
+fonttools varLib.instancer ../fonts/7families/deca/$vf HEXP=0 --output ../fonts/7families/deca/LexendDeca[wght].ttf 
+rm ../fonts/7families/deca/$vf
 
-ttfs=$(ls $ttDir/*-Regular.ttf)
-for font in $ttfs
-do
-	gftools fix-weightclass $font
-	[ -f $font.fix ] && mv $font.fix $font
-done 
+echo ".
+BUILD EXA
+."
+gftools builder exa.yaml
+fonttools varLib.instancer ../fonts/7families/exa/$vf HEXP=40 --output ../fonts/7families/exa/LexendExa[wght].ttf 
+rm ../fonts/7families/exa/$vf
+
+echo ".
+BUILD GIGA
+."
+gftools builder giga.yaml
+fonttools varLib.instancer ../fonts/7families/giga/$vf HEXP=56 --output ../fonts/7families/giga/LexendGiga[wght].ttf 
+rm ../fonts/7families/giga/$vf
+
+echo ".
+BUILD MEGA
+."
+gftools builder mega.yaml
+fonttools varLib.instancer ../fonts/7families/mega/$vf HEXP=64 --output ../fonts/7families/mega/LexendMega[wght].ttf 
+rm ../fonts/7families/mega/$vf
+
+echo ".
+BUILD PETA
+."
+gftools builder peta.yaml
+fonttools varLib.instancer ../fonts/7families/peta/$vf HEXP=72 --output ../fonts/7families/peta/LexendPeta[wght].ttf 
+rm ../fonts/7families/peta/$vf
+
+echo ".
+BUILD TERA
+."
+gftools builder tera.yaml
+fonttools varLib.instancer ../fonts/7families/tera/$vf HEXP=80 --output ../fonts/7families/tera/LexendTera[wght].ttf 
+rm ../fonts/7families/tera/$vf
+
+echo ".
+BUILD ZETTA
+."
+gftools builder zetta.yaml
+fonttools varLib.instancer ../fonts/7families/zetta/$vf HEXP=100 --output ../fonts/7families/zetta/LexendZetta[wght].ttf 
+rm ../fonts/7families/zetta/$vf
+
+echo ".
+BUILD ALL STATICS, WEBFONTS, AND 2AXES VF
+."
+
+gftools builder lexend.yaml
 
 
 # echo ".
